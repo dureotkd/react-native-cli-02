82f97a216fea36605a11f863be63eff165543126
성민 <kevin@seongmin-ui-MacBookAir.local>
2022-02-23 19:12:14 +0900

    modal

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,4 +1,12 @@
-import {StyleSheet, View, Text, Pressable, TextInput} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Pressable,
+  TextInput,
+  Alert,
+  Dimensions,
+} from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import React, {useCallback, useRef, useState, useEffect} from 'react';
 import {RootStackParamList} from '../../App';
@@ -6,10 +14,14 @@ import {RootStackParamList} from '../../App';
 type AuthScreenProps = NativeStackScreenProps<RootStackParamList, 'Auth'>;
 
 function Auth({navigation}: AuthScreenProps) {
+  const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const emailRef = useRef('');
   const passwordRef = useRef('');
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     emailRef.current.focus();
@@ -32,47 +44,87 @@ function Auth({navigation}: AuthScreenProps) {
     setPassword(text.trim());
   }, []);
 
+  const hideModal = useCallback(event => {
+    setShowModal(false);
+  }, []);
+
   return (
-    <View style={{padding: 12}}>
-      <View style={styles.inputWrapper}>
-        <Text style={styles.label}>이메일</Text>
-        <TextInput
-          style={styles.textInput}
-          ref={emailRef}
-          onChangeText={onChangeEmail}
-        />
+    <>
+      <View style={{padding: 12}}>
+        <View style={styles.inputWrapper}>
+          <Text style={styles.label}>이메일</Text>
+          <TextInput
+            style={styles.textInput}
+            ref={emailRef}
+            onChangeText={onChangeEmail}
+          />
+        </View>
+        <View style={styles.inputWrapper}>
+          <Text style={styles.label}>비밀번호</Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder="비밀번호를 입력해주세요(영문,숫자,특수문자)"
+            placeholderTextColor="#666"
+            importantForAutofill="yes"
+            onChangeText={onChangePassword}
+            value={password}
+            autoComplete="password"
+            textContentType="password"
+            secureTextEntry
+            returnKeyType="send"
+            clearButtonMode="while-editing"
+            ref={passwordRef}
+            onSubmitEditing={onSubmit}
+          />
+        </View>
+        <Pressable style={styles.loginButton} onPress={onSubmit}>
+          <Text>로그인</Text>
+        </Pressable>
+        <Pressable
+          style={styles.loginButton}
+          onPress={() => navigation.navigate('Join')}>
+          <Text>회원가입</Text>
+        </Pressable>
+        <Pressable
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'blue',
+            padding: 12,
+          }}
+          onPress={() => setShowModal(true)}>
+          <Text style={{color: 'white'}}>모달창</Text>
+        </Pressable>
       </View>
-      <View style={styles.inputWrapper}>
-        <Text style={styles.label}>비밀번호</Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder="비밀번호를 입력해주세요(영문,숫자,특수문자)"
-          placeholderTextColor="#666"
-          importantForAutofill="yes"
-          onChangeText={onChangePassword}
-          value={password}
-          autoComplete="password"
-          textContentType="password"
-          secureTextEntry
-          returnKeyType="send"
-          clearButtonMode="while-editing"
-          ref={passwordRef}
-          onSubmitEditing={onSubmit}
-        />
-      </View>
-      <Pressable style={styles.loginButton} onPress={onSubmit}>
-        <Text>로그인</Text>
-      </Pressable>
-      <Pressable
-        style={styles.loginButton}
-        onPress={() => navigation.navigate('Join')}>
-        <Text>회원가입</Text>
-      </Pressable>
-    </View>
+      {showModal ? (
+        <Pressable
+          nativeID="modalOut"
+          style={styles.modalSheet}
+          onPress={hideModal}>
+          <Pressable style={styles.modal}></Pressable>
+        </Pressable>
+      ) : null}
+    </>
   );
 }
 
 const styles = StyleSheet.create({
+  modalSheet: {
+    width: '100%',
+    height: '100%',
+    ...StyleSheet.absoluteFillObject,
+  },
+  modal: {
+    position: 'absolute',
+    backgroundColor: 'orange',
+    top: 50,
+    left: 50,
+    bottom: 50,
+    right: 50,
+    borderRadius: 6,
+  },
   textInput: {
     padding: 5,
     borderBottomWidth: StyleSheet.hairlineWidth,
